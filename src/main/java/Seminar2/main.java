85d50089bd2c33ef82210c5dd12bb919a542a891
Ilya <gordoev@yandex.ru>
2023-12-03 13:32:03 +0300

    Add HW №2

--- src/main/java/Seminar2/main.java
+++ src/main/java/Seminar2/main.java
@@ -1,24 +1,20 @@
 package Seminar2;
 
 
+import Seminar1.Chumka;
+
+import java.time.LocalDate;
+
 public class main {
     public static void main(String[] args) {
-        Cat cat = new Cat();
-        cat.setName("Barsik");
 
-        Dog dog = new Dog();
-        dog.setName("Bobik");
+        Cat cat = new Cat("Barsik",new Illness("Allergy"), "Petr", LocalDate.of(2020,10,10));
+        Dog dog = new Dog("Belka",new Illness("Aerophobia"),"Korolev" ,LocalDate.of(1960,8,19));
+        Fish fish = new Fish("Nemo",new Illness("Allergy"), "Jules Verne", LocalDate.of(1802,1,1));
+        Duck duck =new Duck("Donald",new Illness("Post-traumatic stress disorder"), "Walt", LocalDate.of(1934,1,1));
+        Parrot parrot = new Parrot("Kesha",new Illness("Smoking-related illnesses!"), "Petr", LocalDate.of(1984,1,1));
 
-        Fish fish = new Fish();
-        fish.setName("Nemo");
-
-        Duck duck =new Duck();
-        duck.setName("Donald");
-
-        Parrot parrot = new Parrot();
-        parrot.setName("Kesha");
-
-        Doctor doctor1 = new Doctor("Aibolit", "Therapist", 03, "Teresa" );
+        Doctor doctor1 = new Doctor("Aibolit", "Therapist", 112, "Teresa" );
         Doctor doctor2 = new Doctor("House", "Diagnostician", 911, "Teresa" );
 
         Nurse nurse = new Nurse("Teresa", "Nurse", 112);
